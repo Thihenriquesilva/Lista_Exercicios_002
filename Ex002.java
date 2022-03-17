c1aee1382945a18f352704cfedabb0e20ee37b0a
Thiago Henrique <henriquesilva322@outlook.com>
2022-03-17 10:39:50 -0300

    Finalizado!!

--- Ex002.java
+++ Ex002.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Ex002 {
     public static void main(String[] args) {
         /*
@@ -10,7 +13,35 @@ public class Ex002 {
         //nota >=6 ? 'Aprovado' : 'Reprovado'
         //Compilar javcac -source 8 -target 8 nomeArquivo.java
 
-        
+        double a,b,c, delta, x1, x2;
 
+        Scanner scn = new Scanner(System.in);
+
+        System.out.printf("Digite o coeficiente A:");
+        a = scn.nextDouble();
+        System.out.printf("Digite o coeficiente B:");
+        b = scn.nextDouble();
+        System.out.printf("Digite o coeficiente C:");
+        c = scn.nextDouble();
+        delta = (b*b) - 4*a*c;
+        
+        if(a == 0)
+            System.out.println("Não é equação de segundo grau!!");
+        else if(delta > 0){
+            x1 = (-b + Math.sqrt(delta)) / (2*a);
+            x2 = (-b - Math.sqrt(delta)) / (2*a);
+            System.out.println(
+                String.format("X1 = %.1f", x1));
+            System.out.println(
+                String.format("X2 = %.1f", x2));
+        }
+        else if(delta == 0){
+            x1 = -b /(2+a);
+            System.out.println(String.format("X1 = %.1f", x1));
+        }
+        else //delta negativo
+            System.out.println("Não tem raizes reais.");
+
+            scn.close();
     }
 }
